2c9656173e128392f488f2e00b8d5fed1c24fd3a
hlsalazar <hlsalazar@espe.edu.ec>
2024-07-03 14:59:08 -0500

    adicion firebase 5

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+
+export const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+export const analytics = getAnalytics(app);
